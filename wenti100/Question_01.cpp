9c990465a9f06451f0254a4f1dd6f60e3f600b86
lwqworld <46948146+lwqworld@users.noreply.github.com>
2020-06-22 13:20:21 +0800

    2

--- wenti100/Question_01.cpp
+++ wenti100/Question_01.cpp
@@ -47,9 +47,9 @@ cv::Mat Binarize(cv::Mat gray_img, int thr)
 			if (gray_img.at<uchar>(y, x) > thr) {
 				out.at<uchar>(y, x) = 255;
 			}
-			else {
+			/*else {
 				out.at<uchar>(y, x) = 0;
-			}
+			}*/
 		}
 	}
 	return out;
@@ -61,48 +61,76 @@ cv::Mat Otsu(cv::Mat gray_img)
 	int h = gray_img.rows;
 	int size = h * w;
 
+	const int GrayScale = 256;
+	int pixCount[GrayScale] = { 0 };//每个灰度值所占个数
+	double pixPro[GrayScale] = { 0 };//每个灰度值所占比例
+
 	int thr = 0;//阈值
 	int val = 0;
 
-	int sb = 0, max_sb = 0;//计算得出的方差值
-	//前景比例w0,前景平均灰度u0
-	//背景比例w1,背景平均灰度u1
+	//背景景比例w0,背景平均灰度u0
+	//前景比例w1,前景平均灰度u1
 	double w0 = 0, w1 = 0, u0 = 0, u1 = 0;
-	for (int t = 0; t < 255; t++) {
-		w0 = 0; w1 = 0;
-		u0 = 0; u1 = 0;
+	
+	double sb = 0, max_sb = 0;//计算得出的方差值
 
-		//像素值统计区分
-		for (int y = 0; y < h; y++) {
-			for (int x = 0; x < w; x++) {
-				val = gray_img.at<uchar>(y, x);
-				if (val < thr) {
-					w0++;
-					u0 += val;//SUM
-				}
-				else {
-					w1++;
-					u1 += val;//SUM
-				}
+	//像素值统计
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			pixCount[ gray_img.at<uchar>(y, x)]++;
+				
+		}
+	}
+	//灰度值所占比例
+	for (int i = 0; i < GrayScale; i++)
+	{
+		pixPro[i] = pixCount[i] * 1.0 / size;
+	}
+	//遍历所有灰度级
+	for (int t = 0; t < GrayScale; t++) {
+		w0 = w1 = u0 = u1 = 0;
+		for (int j = 0; j < GrayScale; j++) {
+			if (j <= t)//背景
+			{
+				//计算平均灰度,比例
+				w0 += pixPro[j];
+				u0 += j * pixPro[j];
+			}
+			else//前景 
+			{
+				//计算平均灰度,比例
+				w1 += pixPro[j];
+				u1 += j * pixPro[j];
 			}
 		}
+		u0 = u0 / w0;
+		u1 = u1 / w1;
+		//方差评估
+		sb = (double)(w0 * w1 * pow((u0 - u1), 2));
+		//前景与背景方差越大区分越明显
+		if (sb > max_sb) {
+			max_sb = sb;
+			thr = t;
+		}
 
-		//计算平均灰度,比例
-		u0 /= w0;
-		u1 /= w1;
-		w0 /= size;
-		w1 /= size;
-		//
 	}
+		
 	
+	std::cout << "Optimal threshold:" << thr << std::endl;
+
 	cv::Mat out = cv::Mat::zeros(h, w, CV_8UC1);
 
 	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++) {
-
+			if (gray_img.at<uchar>(y, x) > thr) {
+				out.at<uchar>(y, x) = 255;
+			}
+			/*else {
+				out.at<uchar>(y, x) = 0;
+			}*/
 		}
 	}
-	return cv::Mat();
+	return out;
 }
 
 cv::Mat HSVBGR(cv::Mat img, int s)
--- wenti100/main.cpp
+++ wenti100/main.cpp
@@ -3,6 +3,14 @@
 
 
 int main(int argc, char* argv) {
+	cv::Mat img = cv::imread("../test_picture/test3.jpg", cv::IMREAD_COLOR);
+	cv::Mat gray = Grayscale(img);
+	cv::imshow("gray", gray);
+	
+	cv::Mat  out = Otsu(gray);
+	cv::imshow("out",out);
 
+	cv::waitKey(0);
+	cv::destroyAllWindows();
 	return 0;
 }
