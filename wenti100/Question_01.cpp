0088ae4a0af996f379d4f12458a5a64552de334e
lwqworld <46948146+lwqworld@users.noreply.github.com>
2020-06-22 10:42:57 +0800

    2

---
+++ wenti100/Question_01.cpp
@@ -0,0 +1,31 @@
+#include"Question_01.h"
+
+
+cv::Mat chanel_swap(cv::Mat img, int c1, int c2)
+{
+	int w = img.cols;
+	int h = img.rows;
+
+	cv::Mat out = img.clone();
+	return out;
+}
+
+cv::Mat Grayscale(cv::Mat img)
+{
+	return cv::Mat();
+}
+
+cv::Mat Thresholding(cv::Mat gray_img, int thr)
+{
+	return cv::Mat();
+}
+
+cv::Mat Otsu(cv::Mat gray_img)
+{
+	return cv::Mat();
+}
+
+cv::Mat HSVBGR(cv::Mat img, int s)
+{
+	return cv::Mat();
+}
---
+++ wenti100/Question_01.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<opencv2/opencv.hpp>
+#include<opencv2/core.hpp>
+#include<iostream>
+/*
+	功能：通道转换
+	输入：img彩色图像；c1，c2转换的通道名；
+	输出：out转换后图片；
+*/
+cv::Mat chanel_swap(cv::Mat img, int c1, int c2);
+/*
+	功能：灰度化
+	输入：img彩色图像；
+	输出：out转换后图片；
+*/
+cv::Mat  Grayscale(cv::Mat img);
+/*
+	功能：二值化
+	输入：gray_img灰度图像；
+	输出：out转换后图片；
+*/
+cv::Mat Thresholding(cv::Mat gray_img, int thr);
+/*
+	功能：大津二值化
+	输入：gray_img灰度图像；
+	输出：out转换后图片；
+*/
+cv::Mat Otsu(cv::Mat gray_img);
+/*
+	功能：HSV变换
+	输入：img彩色图像；HSV/BGR转换格式
+	输出：out转换后图片；
+*/
+cv::Mat HSVBGR(cv::Mat img, int s);
